12ccc048a07098cc9587a94cc80cdb68d7c05569
tcknapp <trevor.knapp.1991@gmail.com>
2018-10-20 11:48:40 -0700

    href

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,8 @@ import "./Navbar.css";
 
 const Navbar = props => (
   <nav className= "navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="/clicky-game/">
-        </a>
+        <div className="navbar-brand">
+        </div>
           <p className="navbar-right" id="currentscore">Score: {props.score}</p>
   </nav>
 );
