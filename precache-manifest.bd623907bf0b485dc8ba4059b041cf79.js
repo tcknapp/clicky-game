a50373f0409d2258d563aeee3518549c1338ea2d
tcknapp <trevor.knapp.1991@gmail.com>
2018-10-20 12:25:41 -0700

    Updates

---
+++ precache-manifest.bd623907bf0b485dc8ba4059b041cf79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dfac5801410670f69876944802f095f",
+    "url": "/clicky-game/static/media/mktitle.0dfac580.png"
+  },
+  {
+    "revision": "e0816bc3df98f206fb142a0ec3b8ba5f",
+    "url": "/clicky-game/static/media/mklogo.e0816bc3.png"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "5cdfbb3d66c07857934f",
+    "url": "/clicky-game/static/js/main.5cdfbb3d.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "5cdfbb3d66c07857934f",
+    "url": "/clicky-game/static/css/main.de977015.chunk.css"
+  },
+  {
+    "revision": "0e2aa02803d2f708d68f5afdf259b526",
+    "url": "/clicky-game/index.html"
+  }
+];
